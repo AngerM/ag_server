dcc59f19b5aabd4a2b9211485ff165df0c43f36e
Matt Anger <AngerMCS@gmail.com>
2021-02-15 14:31:14 -0800

    base guice setup

--- app/src/main/kotlin/dev/angerm/App.kt
+++ app/src/main/kotlin/dev/angerm/App.kt
@@ -3,10 +3,23 @@
  */
 package dev.angerm
 
+import com.google.inject.AbstractModule
+import com.google.inject.Guice
 import com.linecorp.armeria.server.Server
+import com.linecorp.armeria.server.ServerBuilder
+
+class App: AbstractModule() {
+    private val sb = Server.builder()
+    override fun configure() {
+       bind(ServerBuilder::class.java).toInstance(sb)
+    }
+}
 
 fun main() {
-    val sb = Server.builder()
+    val injector = Guice.createInjector(
+        App()
+    )
+    val sb = injector.getInstance(ServerBuilder::class.java)
     val server = sb.build()
     server.start().join()
 }
