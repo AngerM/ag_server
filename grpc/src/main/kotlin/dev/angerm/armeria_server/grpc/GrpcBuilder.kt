c16567cc58a3f76555e9790132f9a96fb04c1e53
Matt Anger <AngerMCS@gmail.com>
2021-02-18 19:28:43 -0800

    more cleanups

--- grpc/src/main/kotlin/dev/angerm/armeria_server/grpc/GrpcBuilder.kt
+++ grpc/src/main/kotlin/dev/angerm/armeria_server/grpc/GrpcBuilder.kt
@@ -1,4 +1,23 @@
 package dev.angerm.armeria_server.grpc
 
-class GrpcBuilder {
+import com.google.inject.Inject
+import com.linecorp.armeria.server.ServerBuilder
+import com.linecorp.armeria.server.grpc.GrpcService
+import dev.angerm.armeria_server.ArmeriaAddon
+import io.grpc.ServerServiceDefinition
+
+class GrpcBuilder: ArmeriaAddon {
+    private val builder = GrpcService.builder()
+    @Inject(optional = true) private val bindableServices: List<ServerServiceDefinition> = listOf()
+
+    private fun build(): GrpcService {
+        bindableServices.forEach {
+            builder.addService(it)
+        }
+        return builder.build()
+    }
+
+    override fun apply(builder: ServerBuilder) {
+        builder.service(build())
+    }
 }
--- grpc/src/main/kotlin/dev/angerm/armeria_server/grpc/GrpcModule.kt
+++ grpc/src/main/kotlin/dev/angerm/armeria_server/grpc/GrpcModule.kt
@@ -1,6 +1,13 @@
 package dev.angerm.armeria_server.grpc
 
 import com.google.inject.AbstractModule
+import com.google.inject.Inject
+import com.google.inject.Provides
 
 class GrpcModule: AbstractModule() {
+    @Provides
+    @Inject
+    fun getBuilder(
+        impl: GrpcBuilder
+    ) = impl
 }
