6f00ff3f6f1050beb9b02e17970646c5439290e8
Matt Anger <AngerMCS@gmail.com>
2021-02-18 19:15:40 -0800

    cleanups and additions

---
+++ grpc/src/main/kotlin/dev/angerm/armeria_server/grpc/GrpcBuilder.kt
@@ -0,0 +1,4 @@
+package dev.angerm.armeria_server.grpc
+
+class GrpcBuilder {
+}
---
+++ grpc/src/main/kotlin/dev/angerm/armeria_server/grpc/GrpcModule.kt
@@ -0,0 +1,6 @@
+package dev.angerm.armeria_server.grpc
+
+import com.google.inject.AbstractModule
+
+class GrpcModule: AbstractModule() {
+}
