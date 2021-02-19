69e1ea20d5703fc479b55c3f0e28828207712f6d
Matt Anger <AngerMCS@gmail.com>
2021-02-18 19:36:03 -0800

    cleanups

--- grpc/src/main/kotlin/dev/angerm/armeria_server/grpc/GrpcBuilder.kt
+++ grpc/src/main/kotlin/dev/angerm/armeria_server/grpc/GrpcBuilder.kt
@@ -1,15 +1,22 @@
 package dev.angerm.armeria_server.grpc
 
 import com.google.inject.Inject
+import com.google.inject.name.Named
 import com.linecorp.armeria.server.ServerBuilder
 import com.linecorp.armeria.server.grpc.GrpcService
 import dev.angerm.armeria_server.ArmeriaAddon
 import io.grpc.ServerServiceDefinition
+import io.grpc.ServerInterceptor
+import io.grpc.ServerInterceptors
 import io.grpc.protobuf.services.ProtoReflectionService
 
 class GrpcBuilder : ArmeriaAddon {
+    companion object {
+        const val GLOBAL_INTERCEPTORS = "Global"
+    }
     private val builder = GrpcService.builder()
-    @Inject(optional = true) private val bindableServices: List<ServerServiceDefinition> = listOf()
+    @Inject(optional = true) private val bindableServices: Set<ServerServiceDefinition> = setOf()
+    @Inject(optional = true) @Named(GLOBAL_INTERCEPTORS) private val defaultInterceptors: Set<ServerInterceptor> = setOf()
 
     init {
         builder.addService(ProtoReflectionService.newInstance())
@@ -17,7 +24,9 @@ class GrpcBuilder : ArmeriaAddon {
 
     private fun build(): GrpcService {
         bindableServices.forEach {
-            builder.addService(it)
+            builder.addService(
+                ServerInterceptors.intercept(it, defaultInterceptors.toList())
+            )
         }
         return builder.build()
     }
