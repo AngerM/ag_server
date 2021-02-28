fb4ecf7e36dd93e70239db27db22afdcf6d9f002
Matt Anger <AngerMCS@gmail.com>
2021-02-27 19:46:22 -0800

    add db config test

---
+++ database/src/test/kotlin/dev/angerm/ag_server/database/DatabaseModuleTest.kt
@@ -0,0 +1,31 @@
+package dev.angerm.ag_server.database
+
+import com.uchuhimo.konf.Config
+import com.uchuhimo.konf.source.yaml
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class DatabaseModuleTest {
+    @Test
+    fun testConfigParsing() {
+        val config = Config {
+            this.addSpec(DatabaseSpec)
+        } .from.yaml.string(
+            """
+               database:
+                 primary:
+                   protocol: postgres
+                   hostname: localhost
+                   port: 5379
+                   database: test
+           """.trimIndent()
+        ).from.env()
+        val dbConfigs = config[DatabaseSpec.database]
+        assertEquals(1, dbConfigs.size)
+        val first = dbConfigs[dbConfigs.keys.first()]!!
+        assertEquals("localhost", first.hostname)
+        assertEquals("postgres", first.protocol)
+        assertEquals(5379, first.port)
+        assertEquals("test", first.database)
+    }
+}
