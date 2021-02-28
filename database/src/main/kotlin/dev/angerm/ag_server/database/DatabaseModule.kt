1c77698185f685a633976c32dce2ee745afaf559
Matt Anger <AngerMCS@gmail.com>
2021-02-27 18:56:13 -0800

    db connection pool module

--- database/src/main/kotlin/dev/angerm/ag_server/database/DatabaseModule.kt
+++ database/src/main/kotlin/dev/angerm/ag_server/database/DatabaseModule.kt
@@ -1,5 +1,40 @@
 package dev.angerm.ag_server.database
 
 import com.google.inject.AbstractModule
+import com.google.inject.Inject
+import com.google.inject.Provides
+import com.google.inject.Singleton
+import com.uchuhimo.konf.Config
+import io.r2dbc.pool.ConnectionPoolConfiguration
+import io.r2dbc.spi.ConnectionFactories
+import io.r2dbc.spi.ConnectionFactoryOptions
+import io.r2dbc.spi.Option
+import org.springframework.r2dbc.core.DatabaseClient
 
-class DatabaseModule: AbstractModule()
+class DatabaseModule: AbstractModule() {
+    @Provides
+    @Inject
+    @Singleton
+    fun getDbs(
+        conf: Config,
+    ): Map<String, DatabaseClient> {
+        val dbConfigs = conf[DatabaseSpec.database]
+        return dbConfigs.map {
+            val dbConf = it.value
+            val cfo = ConnectionFactoryOptions.builder().apply {
+                this.option(ConnectionFactoryOptions.DRIVER, "pool")
+                this.option(ConnectionFactoryOptions.PROTOCOL, dbConf.protocol)
+                this.option(ConnectionFactoryOptions.DATABASE, dbConf.database)
+                this.option(ConnectionFactoryOptions.HOST, dbConf.hostname)
+                this.option(ConnectionFactoryOptions.PORT, dbConf.port)
+                this.option(ConnectionFactoryOptions.SSL, dbConf.ssl)
+                this.option(ConnectionFactoryOptions.USER, dbConf.username)
+                this.option(ConnectionFactoryOptions.PASSWORD, System.getenv(dbConf.passwordEnvVar))
+                this.option(Option.valueOf("initialSize"), dbConf.poolInitialSize )
+                this.option(Option.valueOf("maxSize"), dbConf.poolMaxSize )
+            }.build()
+            val cf = ConnectionFactories.get(cfo)
+            it.key to DatabaseClient.create(cf)
+        }.toMap()
+    }
+}
--- database/src/main/kotlin/dev/angerm/ag_server/database/DatabaseSpec.kt
+++ database/src/main/kotlin/dev/angerm/ag_server/database/DatabaseSpec.kt
@@ -2,12 +2,17 @@ package dev.angerm.ag_server.database
 
 import com.uchuhimo.konf.ConfigSpec
 
-class DatabaseSpec: ConfigSpec("") {
+object DatabaseSpec: ConfigSpec("") {
     data class DatabaseConfig(
-        val dbType: String,
+        val protocol: String,
         val hostname: String,
+        val port: Int,
+        val database: String,
         val username: String = "",
         val passwordEnvVar: String = "",
+        val ssl: Boolean = false,
+        val poolInitialSize: Int = 10,
+        val poolMaxSize: Int = 10,
     )
     val database by optional(mapOf<String, DatabaseConfig>())
 }
