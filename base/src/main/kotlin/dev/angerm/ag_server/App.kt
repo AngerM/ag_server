91de7b067bb84ea51e9eebb384ed8bb74ee5a7b7
Matt Anger <AngerMCS@gmail.com>
2021-04-20 20:52:48 -0700

    more comments

--- base/src/main/kotlin/dev/angerm/ag_server/App.kt
+++ base/src/main/kotlin/dev/angerm/ag_server/App.kt
@@ -7,6 +7,9 @@ import com.linecorp.armeria.server.ServerBuilder
 import dev.angerm.ag_server.http.HttpHandler
 import java.util.concurrent.CompletableFuture
 
+/**
+ * The App interface is mainly here to allow Guice proxying during instantiation
+ */
 interface App {
     fun start(): CompletableFuture<*>
     fun stop(): CompletableFuture<*>
@@ -14,6 +17,14 @@ interface App {
     fun port(): Int
 }
 
+/**
+ * Actual wrapper class for the Armeria server and its addons
+ *
+ * @param builder an Armeria ServerBuilder class
+ * @param handlers the HttpHandlers to register
+ * @param decorators decorators to add to this server (middleware)
+ * @param addons other addons you would like the wrapper class to control
+ */
 class AppImpl @Inject constructor(
     builder: ServerBuilder,
     handlers: Set<HttpHandler>,
