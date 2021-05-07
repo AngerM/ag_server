c37cf20f2e654c17fa06179e6d4bf3a972bc7085
Matt Anger <AngerMCS@gmail.com>
2021-05-06 20:44:43 -0700

    refactor tests, add grpc test

--- grpc/src/test/kotlin/dev/angerm/ag_server/grpc/GrpcModuleTest.kt
+++ grpc/src/test/kotlin/dev/angerm/ag_server/grpc/GrpcModuleTest.kt
@@ -1,23 +1,23 @@
 package dev.angerm.ag_server.grpc
 
+import com.linecorp.armeria.client.Clients
 import dev.angerm.ag_server.App
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import com.linecorp.armeria.client.Clients;
 import io.grpc.health.v1.HealthCheckRequest
 import io.grpc.health.v1.HealthCheckResponse
 import io.grpc.health.v1.HealthGrpc
-
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class GrpcModuleTest {
     @Test fun testHealth() = App.testServer(GrpcModule()) { server ->
         val client = Clients.newClient(
-           "gproto+http://localhost:${server.port()}",
+            "gproto+http://localhost:${server.port()}",
             HealthGrpc.HealthBlockingStub::class.java
         )
-        val healthy = client.check(HealthCheckRequest.newBuilder().apply {
-
-        }.build())
+        val healthy = client.check(
+            HealthCheckRequest.newBuilder().apply {
+            }.build()
+        )
         assertEquals(HealthCheckResponse.ServingStatus.SERVING, healthy.status)
     }
 }
