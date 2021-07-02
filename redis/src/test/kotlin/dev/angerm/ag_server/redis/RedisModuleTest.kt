31455b19a3b8c8a73e8a657202322951677b4fbb
Matt Anger <AngerMCS@gmail.com>
2021-07-01 21:47:04 -0700

    add redis test

---
+++ redis/src/test/kotlin/dev/angerm/ag_server/redis/RedisModuleTest.kt
@@ -0,0 +1,31 @@
+package dev.angerm.ag_server.redis
+
+import dev.angerm.ag_server.App
+import kotlinx.coroutines.future.await
+import redis.embedded.RedisServer
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+class RedisModuleTest {
+    @Test fun testRedisClient() = App.testServer(
+        RedisModule(),
+        rawYamlConfig = """
+        redis:
+            default:
+                uri: "redis://localhost:6379"
+        """.trimIndent()
+    ) { server ->
+        val redis = RedisServer()
+        try {
+            redis.start()
+            val container = server.getInjector()?.getInstance(RedisContainer::class.java)
+            val client = container?.redisClients?.get("default")
+            assertNotNull(client)
+            val ping = client?.ping()?.await()
+            assertEquals("PONG", ping)
+        } finally {
+            redis.stop()
+        }
+    }
+}
