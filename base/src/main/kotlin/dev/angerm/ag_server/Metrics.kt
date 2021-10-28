e40ff3d9ab0cd33f4be218f57c928e3f015ad235
Matt Anger <AngerMCS@gmail.com>
2021-10-27 22:10:38 -0700

    manually specify buckets

--- base/src/main/kotlin/dev/angerm/ag_server/Metrics.kt
+++ base/src/main/kotlin/dev/angerm/ag_server/Metrics.kt
@@ -17,8 +17,31 @@ class Metrics @Inject constructor(registry: CollectorRegistry) {
 
     val httpLatency = Histogram.Builder()
         .name("http_latency")
-        .help("http request latency in ms")
-        .exponentialBuckets(1.0, 2.0, 20)
+        .help("http request latency in seconds")
+        .buckets(
+            0.001,
+            0.005,
+            0.010,
+            0.025,
+            0.050,
+            0.075,
+            0.100,
+            0.200,
+            0.300,
+            0.400,
+            0.500,
+            0.600,
+            0.700,
+            0.800,
+            0.900,
+            1.0,
+            2.0,
+            5.0,
+            10.0,
+            20.0,
+            30.0,
+            60.0
+        )
         .labelNames(
             "http_method",
             "request_path",
